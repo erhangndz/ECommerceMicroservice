2e77c25034fd972aa16b57ba50928a450b841825
Erhan Gündüz <erhangndz@live.com>
2025-05-16 21:47:59 +0300

    Add ECommerce.Message project for user messaging service
    This commit introduces a new project, `ECommerce.Message`, to the ECommerce microservice architecture. It includes a web application for handling user messages, with necessary configurations in the solution file.
    
    Key changes:
    - Updated `Ports.txt` with new service endpoints.
    - Modified `launchSettings.json` for application URLs.
    - Added configuration settings in `appsettings.Development.json` and `appsettings.json`.
    - Created `ECommerce.Message.csproj` with required SDK and package references.
    - Defined `IUserMessageService` interface and its implementation in `UserMessageService.cs`.
    - Established `AppDbContext` for database management and added a `DbSet` for `UserMessage`.
    - Implemented `MessagesController` for CRUD operations on user messages.
    - Introduced DTOs for data exchange: `CreateUserMessageDto`, `ResultUserMessageDto`, and `UpdateUserMessageDto`.
    - Created `UserMessage` entity class for database representation.
    - Added EF Core migrations for the initial `UserMessages` table schema.
    - Generated `AppDbContextModelSnapshot.cs` for migration tracking.
    
    These changes enable efficient handling of user messages with a PostgreSQL backend.

---
+++ Services/Message/ECommerce.Message/Controllers/MessagesController.cs
@@ -0,0 +1,52 @@
+﻿using ECommerce.Message.DTOs;
+using ECommerce.Message.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Message.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController(IUserMessageService _userMessageService) : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var values = await _userMessageService.GetAllAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var value = await _userMessageService.GetByIdAsync(id);
+            if (value is null)
+            {
+                return BadRequest("Message Not Found");
+            }
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUserMessageDto messageDto)
+        {
+            await _userMessageService.CreateAsync(messageDto);
+            return Ok("Message Created");
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, UpdateUserMessageDto messageDto)
+        {
+            await _userMessageService.UpdateAsync(id, messageDto);
+            return Ok("Message Updated");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _userMessageService.DeleteAsync(id);
+            return Ok("Message Deleted");
+        }
+    }
+}
---
+++ Services/Message/ECommerce.Message/Services/UserMessageService.cs
@@ -0,0 +1,55 @@
+﻿using ECommerce.Message.Context;
+using ECommerce.Message.DTOs;
+using ECommerce.Message.Entities;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Message.Services
+{
+    public class UserMessageService(AppDbContext _context) : IUserMessageService
+    {
+        public async Task CreateAsync(CreateUserMessageDto messageDto)
+        {
+            var message = messageDto.Adapt<UserMessage>();
+            await _context.AddAsync(message);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var message = await _context.UserMessages.FindAsync(id);
+            _context.Remove(message);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<ResultUserMessageDto>> GetAllAsync()
+        {
+            var values = await _context.UserMessages.AsNoTracking().ToListAsync();
+
+            return values.Adapt<List<ResultUserMessageDto>>();
+        }
+
+        public async Task<ResultUserMessageDto> GetByIdAsync(int id)
+        {
+            var value = await _context.UserMessages.FindAsync(id);
+            return value.Adapt<ResultUserMessageDto>();
+        }
+
+        public async Task UpdateAsync(int id,UpdateUserMessageDto messageDto)
+        {
+            var message = await _context.UserMessages.FindAsync(id);
+            if (message is null)
+            {
+                throw new Exception("Message Not Found");
+            }
+
+            message.NameSurname = messageDto.NameSurname;
+            message.Email= messageDto.Email;
+            message.MessageBody = messageDto.MessageBody;
+            message.Subject= messageDto.Subject;
+            message.IsRead= messageDto.IsRead;
+            _context.Update(message);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
