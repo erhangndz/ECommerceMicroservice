e41072d2c1e5ef5f4197f8dcfd3fc0ebe6869fa7
Erhan Gündüz <erhangndz@live.com>
2025-05-20 22:46:06 +0300

    updated

--- Services/Catalog/ECommerce.Catalog/Program.cs
+++ Services/Catalog/ECommerce.Catalog/Program.cs
@@ -2,12 +2,18 @@ using ECommerce.Catalog.Repositories;
 using ECommerce.Catalog.Services.CategoryServices;
 using ECommerce.Catalog.Services.ProductServices;
 using ECommerce.Catalog.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+{
+    opt.Authority = builder.Configuration["IdentityServerUrl"];
+    opt.Audience = "catalog_resource";
+});
 
 builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection(nameof(DatabaseSettings)));
 
@@ -18,7 +24,11 @@ builder.Services.AddScoped<IProductService, ProductService>();
 
 builder.Services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter());
+    
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -33,7 +43,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
